e4dea43e7768c04feb759026587e826fed257537
abdulmoiz001CD <abdulmoizs2005@gmail.com>
2025-06-10 18:51:28 +0500

    feat: setup Gemini agent and implemented basic streaming on ChanLit

---
+++ Third_class/main.py
@@ -0,0 +1,43 @@
+import os
+from agents import Agent, Runner,ItemHelpers, AsyncOpenAI, OpenAIChatCompletionsModel,RunConfig
+import random
+from dotenv import load_dotenv
+
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
+
+external_client = AsyncOpenAI(
+    api_key= gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model= "gemini-2.0-flash",
+    openai_client= external_client
+)
+
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+
+async def main():
+    agents =Agent(
+        instructions="You are a Helper assistent. First determine How many Jokes to Tell, then provide Jokes",
+        tools=[],
+    )
+    result = Runner.run_streamed(agents, input= "Hello")
+    async for event in result.stream_event():
+        if event.item.type == "tool_call_output_item":
+            print(f"Total output: {event.item.type}")
+        elif event.item.type == "message_output_item":
+            print(Item Helper.text_message_output(event.item))
+
+async.run(main())
+
